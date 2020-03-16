df01870c961dd45e64b8c66e60add445a07dc83a
Karim Slimani <karim@playmedia.fr>
2020-03-16 13:47:52 +0100

    chore: rebuild dist plugin

--- dist/myplayback.js
+++ dist/myplayback.js
@@ -1386,7 +1386,7 @@ exports["default"] = void 0;
 
 var _clappr = __webpack_require__(94);
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -1409,9 +1409,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
 // Note: HTML5Video playback class extends Playback class
-var MyPlayback =
-/*#__PURE__*/
-function (_HTML5Video) {
+var MyPlayback = /*#__PURE__*/function (_HTML5Video) {
   _inherits(MyPlayback, _HTML5Video);
 
   _createClass(MyPlayback, [{
